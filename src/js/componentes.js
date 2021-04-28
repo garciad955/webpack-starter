a6a6b3cb789fc219d2b922a131fce1dc7d06100b
Daniel <garciad955@gmail.con>
2021-04-28 00:56:50 -0500

    borrado

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -4,7 +4,7 @@ export const saludar = ( nombre ) => {
     console.log('Creando etiqueta H1');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre}, cómo estas mi perrote UwUr?`;
+    h1.innerText = `Hola, ${nombre}, cómo estas mi perrote UwUr ya jalaaa?`;
 
     document.body.append( h1 );
 }
