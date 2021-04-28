134569bf0b205db25387602773ab9d3b3c08f2b8
Daniel <garciad955@gmail.con>
2021-04-28 00:50:02 -0500

    Docs creado

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -4,7 +4,7 @@ export const saludar = ( nombre ) => {
     console.log('Creando etiqueta H1');
 
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola, ${nombre}, cómo estas mi perrote?`;
+    h1.innerText = `Hola, ${nombre}, cómo estas mi perrote UwUr?`;
 
     document.body.append( h1 );
 }
